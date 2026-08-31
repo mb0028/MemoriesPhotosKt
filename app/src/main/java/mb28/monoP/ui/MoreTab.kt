4729bc86b5a82be3fb27c013a1bcba58261c0325
mb28 <244608971+mb0028@users.noreply.github.com>
2026-08-31 05:25:34 +0330

    Improve favorites view

--- app/src/main/java/mb28/monoP/ui/MoreTab.kt
+++ app/src/main/java/mb28/monoP/ui/MoreTab.kt
@@ -5,7 +5,8 @@ import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
@@ -14,13 +15,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
 import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
@@ -29,64 +33,118 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.skydoves.cloudy.cloudy
+import com.skydoves.cloudy.rememberSky
+import com.skydoves.cloudy.sky
 import mb28.monoP.R
 import mb28.monoP.core.PhotosListVM
 import mb28.monoP.core.Settings
 import mb28.monoP.core.createOrGetThumbnail
+import mb28.monoP.core.getComment
 import mb28.monoP.core.openPhoto
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MoreTab(modifier: Modifier = Modifier, photosVM: PhotosListVM) {
     val context = LocalActivity.current!!
-    Column(
-        modifier.padding(bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding() + 75.dp)
+    LazyColumn(
+        modifier,
+        contentPadding = PaddingValues(bottom =
+            WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding() + 75.dp)
     ) {
-        val cst = rememberCarouselState(0) { photosVM.photosList.count()
-            .coerceAtMost(Settings.specialSectionsCount) }
-        HorizontalMultiBrowseCarousel(
-            state = cst,
-            preferredItemWidth = 200.dp,
-            itemSpacing = 10.dp,
-            modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
-        ) { i ->
-            Image(
-                BitmapFactory.decodeFile(createOrGetThumbnail(photosVM.photosList[i].path)).asImageBitmap(),
-                null,
-                contentScale = ContentScale.Crop,
+        item {
+            val cst = rememberCarouselState(0) { photosVM.photosList.count()
+                .coerceAtMost(Settings.specialSectionsCount) }
+            HorizontalMultiBrowseCarousel(
+                state = cst,
+                preferredItemWidth = 200.dp,
+                itemSpacing = 10.dp,
+                modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
+            ) { i ->
+                Image(
+                    BitmapFactory.decodeFile(createOrGetThumbnail(photosVM.photosList[i].path)).asImageBitmap(),
+                    null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .height(140.dp)
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(25.dp))
+                        .combinedClickable(
+                            onClick = {
+                                openPhoto(photosVM.photosList[i].path, context)
+                            }
+                        )
+                )
+            }
+        }
+
+        item {
+            Row(
+                Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button({}) {
+                    Text(stringResource(R.string.archive))
+                }
+                Button({}) {
+                    Text(stringResource(R.string.trash))
+                }
+            }
+        }
+
+        item {
+            Text(
+                stringResource(R.string.favorites),
+                fontSize = 38.sp,
+                textAlign = TextAlign.Center,
                 modifier = Modifier
-                    .height(140.dp)
-                    .fillMaxSize()
-                    .clip(RoundedCornerShape(25.dp))
-                    .combinedClickable(
-                        onClick = {
-                            openPhoto(photosVM.photosList[i].path, context)
-                        }
-                    )
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp)
             )
         }
 
-        Row(
-            Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            Button({}) {
-                Text(stringResource(R.string.archive))
-            }
-            Button({}) {
-                Text(stringResource(R.string.trash))
+        items(Settings.favorites.count()) { i ->
+            val path = Settings.favorites[i]
+            val sky = rememberSky()
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+                    .padding(5.dp)
+                    .clip(RoundedCornerShape(25.dp))
+                    .combinedClickable(
+                        onClick = {
+                            openPhoto(path, context)
+                        }
+                    ),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Image(
+                    BitmapFactory.decodeFile(createOrGetThumbnail(path)).asImageBitmap(),
+                    null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .sky(sky)
+                )
+
+                Box(
+                    Modifier
+                        .padding(horizontal = 20.dp, vertical = 10.dp)
+                        .cloudy(sky, 20,
+                            shape = RoundedCornerShape(25.dp),
+                            tint = MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.5f)
+                        ),
+                ) {
+                    Text(
+                        getComment(path),
+                        Modifier.padding(8.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 22.sp,
+                        maxLines = 2
+                    )
+                }
             }
         }
-
-        Text(
-            stringResource(R.string.favorites),
-            fontSize = 38.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 12.dp)
-        )
-
-        MoreTabFavorites()
     }
 }
